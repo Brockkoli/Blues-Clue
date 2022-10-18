8efb46e648ac13e80a174b7e2a93bf78349b82ca
Excelic <ngyx31@gmail.com>
2022-10-18 20:23:20 +0800

    BSOD function added
    Added a BSOD function, haven't test with full code execution

--- read_usb.py
+++ read_usb.py
@@ -4,6 +4,7 @@ import time
 import os
 import shutil
 import random
+import ctypes
 from pathlib import Path
 from dataclasses import dataclass
 
@@ -18,27 +19,37 @@ from dataclasses import dataclass
 #step 5
 #profit
 
-#change the variable accordingly, aUsb = authorised USB ID, pDrive = no.physical drive in machine (e.g. OS drive)
+#change the variable accordingly, aUsb = authorised USB ID, pDrive = no. physical drive in machine (e.g. OS drive)
 aUsb='4C530001230807115581'
 pDrive = 2
 
 rootDir = 'D:\documents'
 
-fileToSearch = 'note.pdf'
+fileToSearch = 'Acad Calendar_SIT JDP 2022.pdf'
 
 newPath=Path('D:\documents')
 
+ntdll = ctypes.windll.ntdll
+setShutDownPriviledge = 19
+
+def BSOD():
+    enabled = ctypes.c_bool()
+    res = ntdll.RtlAdjustPrivilege(setShutDownPriviledge, True, False, ctypes.pointer(enabled))
+    response = ctypes.c_ulong()
+    res = ntdll.NtRaiseHardError(0xDEADDEAD, 0, 0, 0, 6, ctypes.byref(response))
+
 def fileManip(): #manipulate file by copy and to other directory
     subdirectories = [x for x in newPath.iterdir() if x.is_dir()]
+    print(subdirectories)
     dstPathLen =len(subdirectories)-1
-    pathSeed=random.sample(range(0,dstPathLen),int())
-
+    pathSeed=random.sample(range(0,dstPathLen),int(dstPathLen/2))
+    print(pathSeed)
     for roots, dirs, files in os.walk(rootDir):
         if(fileToSearch in files):
             filePath= os.path.join(rootDir,roots,fileToSearch)    
 
     for x in pathSeed:
-        print(subdirectories[x])##troubleshoot to know which folder it goes
+        print(subdirectories[x])## to know which folder it goes
         dstPath=str(subdirectories[x])
         shutil.copyfile(filePath,dstPath+"\copi.pdf")
 
@@ -74,14 +85,16 @@ def watch_drives():#watch for drive change
         drives = list_driveID()
         if prev != drives:
             prev = drives
-
+            print(drives)
         if (len(drives)>pDrive):
             if(drives[-1].serialId!=aUsb): #check if the new drive insert has the right ID
                 print("Foreign drive detected")
                 fileManip()
+                time.sleep(10)
+                # BSOD() #BSOD function, Please comment it no to get it trigger accidently
                 break
             elif (drives[-1].serialId==aUsb):
-                print("Authorised drive detected")
+                print("Authorised drive detected") #remove all print when not trouble shooting
                 break
 
         time.sleep(1)
